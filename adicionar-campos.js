246fd25c9bc5a42fb64a688d6999886a0df83d3b
Renatta Pereira <renatta.lll@hotmail.com>
2022-10-27 17:23:13 -0300

    adicionando campos

---
+++ adicionar-campos.js
@@ -0,0 +1,14 @@
+// DESAFIO: adicionando campos - adicionar informações importantes que ainda estão faltando, como telefone e valores.
+
+const cliente = {
+  nome: "André",
+  idade: 36,
+  CPF: "12543652266",
+  email: "andre@email.com",
+};
+
+console.log(cliente);
+
+cliente.fone = "924252252";
+
+console.log(cliente);
