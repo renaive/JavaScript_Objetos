068293490fab1c63e4a1f7c2a82fa0f1e889814d
Renatta Pereira <renatta.lll@hotmail.com>
2022-10-28 19:10:16 -0300

    puxando relatorio e etc

--- colecionador-gibi.js
+++ colecionador-gibi.js
@@ -1,16 +1,13 @@
 const colecionador = {
   nome: "João do Gibi",
-  idade:41,
+  idade: 41,
   tipocolecao: ["quadrinhos"],
-  contato: "joao@email.com"
- }
+  contato: "joao@email.com",
+};
 
- adicionarTipo:function(propriedade,tipo){
-  this[propriedade].push(tipo)
-}
-for(i = 0; i < 4; i++) {
-  colecionador.adicionarTipo("tipocolecao","HQ"+i)
-}
+console.log(colecionador.nome);
+console.log(colecionador["nome"]);
 
-console.log(colecionador.nome)
-console.log(colecionador["nome"])
+//Usando uma estrutura for conseguimos manipular a propriedade tipocolecao do objeto colecionador, por meio da função adicionarTipo(). Usamos a notação de colchetes junto à palavra reservada this, que faz referência ao próprio objeto. Desse modo, fazemos uso do objeto como um array associativo.
+
+//É importante que conheçamos as diversas maneiras que o JavaScript possui para acessarmos os valores das propriedades de um objeto. Uma dessas formas é usando o conceito de array associativo.
